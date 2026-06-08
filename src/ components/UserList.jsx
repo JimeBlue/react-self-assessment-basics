7e03511db0fd3badcccdc3fcb69147b54d001a9e
JimeBlue <jimeblau@gmail.com>
2026-06-08 12:16:42 +0200

    implement UserList

--- src/ components/UserList.jsx
+++ src/ components/UserList.jsx
@@ -1,11 +1,22 @@
-const UserList = () => {
+const UserList = ({ users }) => {
   // TODO: If users prop is not provided or is an empty array, render:
   //       <div>No users found.</div>
+  if (!users || !Array.isArray(users) || users.length === 0) return <div>No users found.</div>;
+
   // TODO: Otherwise, render a <ul> with one <li> per user
-  // TODO: Each <li> should include:
-  //       - An <img> with `src` set to user's picture and `alt` set to their name
-  //       - The user's name as text
-  return null;
+  return (
+    <ul>
+      {users.map((user) => (
+        <li key={user.id}>
+          {/* TODO: Each <li> should include:
+              - An <img> with `src` set to user's picture and `alt` set to their name
+              - The user's name as text */}
+          <img src={user.picture} alt={user.name} />
+          {user.name}
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default UserList;
