2d6969bc751c1804ba1cf81a0d39b9eb8b803a4e
JimeBlue <jimeblau@gmail.com>
2026-06-08 11:49:57 +0200

    implement Greeting component

--- src/ components/Greeting.jsx
+++ src/ components/Greeting.jsx
@@ -1,8 +1,8 @@
-const Greeting = () => {
-  /* TODO: read `name` from props and render “Hello, <name>!” within a div */
-  /* TODO: if no name is provided, render "Hello, stranger!" */
-  /* TODO: if name is empty, render "Hello, stranger!" */
-  /* TODO: if name is NOT a string, render "Hello???" */
+const Greeting = ({ name }) => {
+  if (name === undefined || name === null) return <div>Hello, stranger!</div>;
+  if (typeof name !== 'string') return <div>Hello???</div>;
+  if (name === '') return <div>Hello, stranger!</div>;
+  return <div>Hello, {name}!</div>;
 };
 
 export default Greeting;
